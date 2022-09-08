0b82730c3771e2ee4bdb14308483240071f6ab17
mubarak <mubbyre6xx@gmail.com>
2022-09-08 13:16:17 -0700

    fix error 03

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * 
+ *
+ *    * Description: 'the program's description'
+ *
+ *     * 
+ *
+ *      * Return: Always 0 (Success)
+ *
+ *       */
 int main(void)
 {
 
