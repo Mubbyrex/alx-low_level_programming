cb93f427a4cfafa46acecc9cdc028acf063f1b63
mubarak <mubbyre6xx@gmail.com>
2022-09-10 10:42:11 -0700

    task 05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main-description
+ * Description - print all digit of the base 10 number
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	if (c < 10)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
